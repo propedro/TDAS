6ede782e6caf575721a4c4b38f5f8b7dbe6d8967
Pedro Peixoto <peixotopedro.pro@gmail.com>
2022-06-17 19:29:08 -0300

    initial commit

---
+++ tdas.py
@@ -0,0 +1,27 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.lang import Builder
+
+Builder.load_string("""
+<ExampleApp>:
+    orientation: "vertical"
+    Button:
+        text: ""
+        on_press: gif.anim_delay = 0.10
+        on_press: gif._coreimage.anim_reset(True)
+
+        Image:
+            id: gif
+            source: 'tdas.zip'
+            center: self.parent.center
+            size: 500, 500
+            allow_stretch: True
+            anim_loop: 30
+""")
+
+class Tdas(App, BoxLayout):
+    def build(self):
+        return self
+
+if __name__ == "__main__":
+    Tdas().run()
